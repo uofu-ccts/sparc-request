f7b08e7d319acb09d675adb012a9049a5b5d770a
shanhong cheng <shanhong.cheng@utah.edu>
2016-07-21 10:12:39 -0600

    contact_mailer_spec

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -9,16 +9,43 @@ RSpec.describe ContactMailer, type: :mailer do
     ContactMailer.contact_us_email(contact_form)
   end
 
+  let(:cc) do
+    if defined? CONTACT_US_EMAIL
+      [CONTACT_US_EMAIL['cc']]
+    else
+      ['sparcrequest@gmail.com']
+    end
+  end
+
+  let(:to) do
+    if defined? CONTACT_US_EMAIL
+      [CONTACT_US_EMAIL['to']]
+    else
+      ['success@musc.edu']
+    end
+  end
+
+  it 'should get mailer from config' do
+    if defined? CONTACT_US_EMAIL
+      expect(CONTACT_US_EMAIL['cc']).to eq 'sparc@chpc.utah.edu'
+      expect(CONTACT_US_EMAIL['to']).to eq 'sparc-admin@lists.utah.edu'
+      expect(mail.cc).to eq ['sparc@chpc.utah.edu']
+      expect(mail.to).to eq ['sparc-admin@lists.utah.edu']
+    else
+      fail 'need define contact_us_email in application.yml'
+    end
+  end
+
   it 'renders subject' do
     expect(mail.subject).to eq 'subject'
   end
 
   it 'renders the receiver email' do
-    expect(mail.to).to eq(['success@musc.edu'])
+    expect(mail.to).to eq(to)
   end
 
   it 'renders the receiver cc email' do
-    expect(mail.cc).to eq(['sparcrequest@gmail.com'])
+    expect(mail.cc).to eq(cc)
   end
 
   it 'renders the sender email' do
