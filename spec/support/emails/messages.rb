7af253e45bb70dab86ee93e7a62b6a6d86ef18af
shanhong cheng <shnahongcheng@gmail.com>
2016-10-09 01:18:39 -0600

    mailer spec

--- spec/support/emails/messages.rb
+++ spec/support/emails/messages.rb
@@ -3,17 +3,17 @@ module EmailHelpers
 
   #### SUBMITTED MESSAGE METHODS ####
   def submitted_service_provider_and_admin_message
-    # Expected message:  
-    # 'A request has been submitted to start services in 
+    # Expected message:
+    # 'A request has been submitted to start services in
     # SPARCRequest and is awaiting your review in SPARCDashboard.'
     expect(mail).to have_xpath("//p[normalize-space(text()) = 'A request has been submitted to start services in SPARCRequest and is awaiting your review in']")
     expect(mail).to have_xpath "//p//a[@href='/dashboard/protocols/#{service_request.protocol.id}'][text()= 'SPARCDashboard.']/@href"
   end
 
   def submitted_general_users_message
-    # Expected user message:  
-    # "A request has been submitted to start services in 
-    # SPARCRequest. Visit SPARCDashboard to view the status or 
+    # Expected user message:
+    # "A request has been submitted to start services in
+    # SPARCRequest. Visit SPARCDashboard to view the status or
     # make any updates to your request."
     expect(mail.body.parts.first.body).to have_xpath("//p[normalize-space(text()) = 'A request has been submitted to start services in SPARCRequest. Visit']")
     expect(mail.body.parts.first.body).to have_xpath "//p//a[@href='/dashboard/protocols/#{service_request.protocol.id}'][text()= 'SPARCDashboard']/@href"
@@ -23,22 +23,22 @@ module EmailHelpers
 
   #### GET A COST ESTIMATE MESSAGE METHODS ####
   def get_a_cost_estimate_service_provider_admin_message(mail_response)
-    # Expected message:  
-    # "A request has been made for a budget review (Get Cost Estimate) in 
-    # SPARCRequest and is awaiting your review in SPARCDashboard. Please ensure services chosen 
-    # are appropriate and can be provided. Communicate any necessary changes to the study team 
+    # Expected message:
+    # "A request has been made for a budget review (Get Cost Estimate) in
+    # SPARCRequest and is awaiting your review in SPARCDashboard. Please ensure services chosen
+    # are appropriate and can be provided. Communicate any necessary changes to the study team
     # and request they “Submit to Start Services” once ready to proceed."
     expect(mail_response).to have_xpath("//p[normalize-space(text()) = 'A request has been made for a budget review (Get Cost Estimate) in SPARCRequest and is awaiting your review in']")
-    expect(mail_response).to have_xpath "//a[@href='/dashboard/protocols/#{service_request.protocol.id}'][text()= 'SPARCDashboard']/@href"
+    expect(mail_response).to have_xpath "//a[@href='/dashboard/protocols/#{service_request.protocol.id}'][text()= 'Administrators/Service Providers, Click Here']/@href"
     expect(mail_response).to have_xpath("//p[normalize-space(text()) = '. Please ensure services chosen are appropriate and can be provided. Communicate any necessary changes to the study team and request they “Submit to Start Services” once ready to proceed.']")
   end
 
   def get_a_cost_estimate_general_users
     # Expected message:
-    # 'A request has been made for a budget review (Get Cost Estimate) in 
-    # SPARCRequest. You can review or edit this request in SPARCDashboard. 
-    # An email has been sent to the service provider(s) to review and ensure 
-    # the services are appropriate. Please note the services will not start 
+    # 'A request has been made for a budget review (Get Cost Estimate) in
+    # SPARCRequest. You can review or edit this request in SPARCDashboard.
+    # An email has been sent to the service provider(s) to review and ensure
+    # the services are appropriate. Please note the services will not start
     # until this request is submitted through SPARCDashboard.'
     expect(mail.body.parts.first.body).to have_xpath("//p[normalize-space(text()) = 'A request has been made for a budget review (Get Cost Estimate) in SPARCRequest. You can review or edit this request in']")
     expect(mail.body.parts.first.body).to have_xpath "//a[@href='/dashboard/protocols/#{service_request.protocol.id}'][text()= 'SPARCDashboard']/@href"
@@ -55,7 +55,7 @@ module EmailHelpers
 
   def message_conclusion(mail_response)
     expect(mail_response).to have_xpath("//p[normalize-space(text()) = 'A list of requested services is attached.']")
-    expect(mail_response).to have_xpath("//p[normalize-space(text()) = 'Please contact the SUCCESS Center at (843) 792-8300 or success@musc.edu for assistance with this process or with any questions you may have.']")
+    expect(mail_response).to have_xpath("//p[normalize-space(text()) = '#{I18n.t('issue_contact')}']")
   end
 
   def does_have_acknowledgments
@@ -77,7 +77,7 @@ module EmailHelpers
     # Should have expected service provider message which is defined under submitted_service_provider_and_admin_message
     # Should have 'Administrators/Service Providers, Click Here' link
     # Should have standard message conclusion
-    # Should NOT show acknowledgments 
+    # Should NOT show acknowledgments
     submitted_service_provider_and_admin_message
     service_provider_and_admin_link(mail_response)
     message_conclusion(mail_response)
@@ -87,7 +87,7 @@ module EmailHelpers
   def submitted_intro_for_general_users
     # Should have expected user message which is defined under submitted_general_users_message
     # Should have standard message conclusion
-    # Should show acknowledgments 
+    # Should show acknowledgments
     submitted_general_users_message
     message_conclusion(mail.body.parts.first.body)
     does_have_acknowledgments
@@ -98,7 +98,7 @@ module EmailHelpers
     # Should have expected service provider message which is defined under get_a_cost_estimate_service_provider_admin_message
     # Should have 'Administrators/Service Providers, Click Here' link
     # Should have standard message conclusion
-    # Should NOT show acknowledgments 
+    # Should NOT show acknowledgments
     get_a_cost_estimate_service_provider_admin_message(mail.body.parts.first.body)
     service_provider_and_admin_link(mail.body.parts.first.body)
     message_conclusion(mail.body.parts.first.body)
@@ -109,7 +109,7 @@ module EmailHelpers
     # Should have expected service provider message which is defined under get_a_cost_estimate_service_provider_admin_message
     # Should have 'Administrators/Service Providers, Click Here' link
     # Should have standard message conclusion
-    # Should NOT show acknowledgments 
+    # Should NOT show acknowledgments
     get_a_cost_estimate_service_provider_admin_message(mail.body)
     service_provider_and_admin_link(mail.body)
     message_conclusion(mail.body)
@@ -119,7 +119,7 @@ module EmailHelpers
   def get_a_cost_estimate_intro_for_general_users
     # Should have expected user message which is defined under get_a_cost_estimate_general_users
     # Should have standard message conclusion
-    # Should show acknowledgments 
+    # Should show acknowledgments
     get_a_cost_estimate_general_users
     message_conclusion(mail.body.parts.first.body)
     does_have_acknowledgments
